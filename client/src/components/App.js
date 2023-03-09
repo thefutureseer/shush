d06d187d4bb03e596cfadec6e4aafb7e605d9815
thefutureseer <danielharrislimos@outlook.com>
2023-03-09 12:07:01 -0800

    online / offline cheker

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import '../css/App.css';
 import Dash from './Dash';
 import text from '../text/basics.txt';
+import StatusBar from './StatusBar';
 
 function App() {
   const jsonData = JSON.stringify(text);
   console.log(jsonData);
   return (
     <div >
+      <StatusBar/>
       <Dash/>
     </div>
   );
