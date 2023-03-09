5ddf512035098d5cbd912560876483369903bff2
thefutureseer <danielharrislimos@outlook.com>
2023-03-09 12:39:45 -0800

    more cleaning for json

---
+++ client/src/text/religionApI.js
@@ -0,0 +1,54 @@
+const religions = [
+  {
+    id: 1,
+    name: 'Animism',
+    description: 'A belief that all things, including objects in nature, have spirits or souls, and should be treated with respect and reverence. This belief is thought to have originated in Africa over 100,000 years ago.'
+  },
+  {
+    id: 2,
+    name: 'Shamanism',
+    description: 'A set of spiritual practices that involve communicating with spirits and other non-physical entities, often through the use of trance states induced by rhythmic drumming or other methods. Shamanism is found in many cultures around the world, and may have originated in Siberia around 30,000 years ago.'
+  },
+  {
+    id: 3,
+    name: 'Ancient Egyptian Religion',
+    description: 'A complex set of beliefs and practices that developed in ancient Egypt around 4,000 years ago, and which involved the worship of a pantheon of gods and goddesses.'
+  },
+  {
+    id: 4,
+    name: 'Hinduism',
+    description: 'A complex and diverse set of religious beliefs and practices that originated in the Indus Valley civilization of ancient India around 4,000 years ago.'
+  },
+  {
+    id: 5,
+    name: 'Judaism',
+    description: 'The monotheistic religion of the Jewish people, dating back to the ancient Israelites in the Middle East around 3,500 years ago.'
+  },
+  {
+    id: 6,
+    name: 'Zoroastrianism',
+    description: 'An ancient Persian religion that originated in Iran around 3,000 years ago, and which influenced many other religions in the region, including Judaism, Christianity, and Islam.'
+  },
+  {
+    id: 7,
+    name: 'Buddhism',
+    description: 'A spiritual tradition that originated in ancient India around 2,500 years ago, and which emphasizes the teachings of the Buddha and the pursuit of enlightenment.'
+  },
+  {
+    id: 8,
+    name: 'Taoism',
+    description: 'A Chinese philosophy and religion that originated in ancient China around 2,500 years ago, and which emphasizes the pursuit of harmony with nature and the cultivation of inner wisdom.'
+  },
+  {
+    id: 9,
+    name: 'Christianity',
+    description: 'A monotheistic religion based on the life and teachings of Jesus of Nazareth, which emerged in the Roman Empire around 2,000 years ago.'
+  },
+  {
+    id: 10,
+    name: 'Islam',
+    description: 'A monotheistic religion founded by the prophet Muhammad in the Arabian Peninsula around 1,400 years ago.'
+  }
+];
+
+export default religions;
