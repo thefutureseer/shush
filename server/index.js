56fcaa67f3ff8d18bc0c2697ec9e2161fc8a0177
thefutureseer <danielharrislimos@outlook.com>
2023-03-11 14:48:48 -0800

    add folders

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const fs = require('fs');
 
-const resolvers = require('./resolve/resolvers');
+const resolvers = require('./graphql/resolvers');
 
 const app = express();
 
@@ -48,54 +48,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
-// Note that either GraphQL Playground or 
-// Altair GraphQL Client can be used at a time. 
-// If you uncomment the playground property, 
-// you should comment out the lines for 
-// Altair GraphQL Client, and vice versa.
-
-
-
-
-
-// const express = require('express');
-// const { graphqlHTTP } = require('express-graphql');
-// const { makeExecutableSchema } = require('graphql-tools');
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-// const fs = require('fs');
-
-// const resolvers = require('./resolve/resolvers');
-
-// const app = express();
-
-// const port = process.env.PORT || 3000;
-
-// // Load type definitions from schema file
-// const typeDefs = fs.readFileSync('./models/typedefs/typedefs.graphql', 'utf8');
-
-// // Connect to MongoDB database
-// mongoose.connect(process.env.DB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => console.log('Connected to database'));
-
-
-// // Create executable schema with type definitions and resolvers
-// const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-// // Create GraphQL route with schema and GraphiQL interface
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   graphiql: true
-// }));
-
-// app.get('/', (req, res) => {
-//   res.send('hello spirit');
-// })
-
-// // Start server
-// app.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
