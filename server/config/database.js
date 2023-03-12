b03988233777f8fa451b14e9622730e817f2f859
thefutureseer <danielharrislimos@outlook.com>
2023-03-12 12:57:08 -0700

    modified same code and add descriptive console.log

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,31 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
+require('dotenv').config();
 
+const dbUri = process.env.DB_URI;
 
-const uri = process.env.DB_URI;
+mongoose.connect(
+  dbUri, 
+  { 
+    useNewUrlParser: true, useUnifiedTopology: true 
+  }
+  );
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false
-};
+const db = mongoose.connection;
 
-mongoose.connect(uri, options)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err.message));
+// descriptive database connection error
+db.on('error', console.error.bind(console, 'connection error:'));
+//open up the database
+// Log database connection status
+db.once('open', function () {
+  console.log('Connected to database');
+});
 
-module.exports = mongoose.connection;
+// Close database connection when Node process ends
+//error says: throw new MongooseError('Connection.prototype.close() no longer accepts a callback');
+// process.on('SIGINT', () => {
+//   db.close(() => {
+//     console.log('Database connection closed');
+//     process.exit(0);
+//   });
+// });
+module.exports = db;
