53e02ad9d9548bceb9a77ea027169e2c5c84bdb8
thefutureseer <danielharrislimos@outlook.com>
2023-03-14 16:20:54 -0700

    add routes or switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import '../css/App.css';
+import './css/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dash from './components/Dash';
-import text from '../text_APIs/basics.txt';
+import text from './text_APIs/basics.txt';
 import StatusBar from './components/StatusBar';
+import MessageBoard from './pages/MessageBoard';
 
 function App() {
   const jsonData = JSON.stringify(text);
   console.log(jsonData);
   return (
+     <BrowserRouter>
     <div >
       <StatusBar/>
-      <Dash/>
+      <Routes>
+        <Route exact path='/' element={<Dash/>}/>
+        <Route path='/message-board' element={<MessageBoard/>}/>
+      </Routes>
     </div>
+     </BrowserRouter>
   );
 }
 
