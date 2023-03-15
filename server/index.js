20751d6ecbb5d5925201da7880a041055cd31304
thefutureseer <danielharrislimos@outlook.com>
2023-03-14 20:45:51 -0700

    add deploy ready

--- server/index.js
+++ server/index.js
@@ -27,12 +27,22 @@ const server = new ApolloServer({
   })
 });
 
+
 async function startServer() {
   await server.start();
   server.applyMiddleware({ app });
-}
+};
 
 startServer();
+
+// Serve React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+};
 
 // Set up Express routes
 app.use('/api/users', userRoutes);
